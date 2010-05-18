7e88a7db65e2a6229586cf62b171d2bf5e83b497
jlippiner <josh@helpishere.org>
2010-05-18 19:11:51 -0400

    upgraded to 2.3.5.  new rvm gemset.  new blog links and badge on homepage

--- config/environment.rb
+++ config/environment.rb
@@ -1,30 +1,24 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem 'compass', :version => '>= 0.10.0'
+  config.gem 'haml', :version => '>=3.0.0'
   config.gem "bj"
-  config.gem 'twitter-auth', :lib => 'twitter_auth'  
   config.gem "json"
   config.gem "ym4r"
   config.gem "sunlight"
   config.gem "RedCloth"
   config.gem "dynamic_reports"
   config.gem "highline"
-  config.gem "haml"
-  config.gem 'compass', :lib => 'compass', :source => "http://gems.github.com", :version => ">=0.10.0.pre4" 
+  config.gem "oauth", :version => '= 0.3.5' 
+  config.gem 'twitter-auth', :lib => 'twitter_auth'
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
