cd1a7c6828c259a553b0d86a29642648d6334719
Noctivity <jlippiner@noctivity.com>
2010-07-13 09:09:23 -0400

    200K campaign

--- config/environment.rb
+++ config/environment.rb
@@ -7,19 +7,6 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'compass', :version => '>= 0.10.0'
-  config.gem 'haml', :version => '>=3.0.0'
-  config.gem "bj"
-  config.gem "json"
-  config.gem "ym4r"
-  config.gem "sunlight"
-  config.gem "RedCloth"
-  config.gem "dynamic_reports"
-  config.gem "highline"
-  config.gem "oauth", :version => '= 0.3.5' 
-  config.gem 'twitter-auth', :lib => 'twitter_auth'
-  
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -40,6 +27,6 @@ Rails::Initializer.run do |config|
   # config.i18n.default_locale = :de
 end
 
-# require 'twitter'
+# # require 'twitter'
 require 'dynamic_reports'
 require 'bj'
